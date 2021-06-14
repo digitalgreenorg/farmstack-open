465551e9c1ea1c329cf06ae7bc16ed17c27591ce
waseem <waseempasha@digitalgreen.org>
2021-06-14 11:05:57 +0530

    Script update for generating SHA-256

--- usage control implementation/prepareUC.sh
+++ usage control implementation/prepareUC.sh
@@ -3,6 +3,7 @@
 CUSTOMER_APP_NAME='merge-csv-nodejs'
 cd 'configs'
 echo 'Building consumer image: '$CUSTOMER_APP_NAME
+export DOCKER_SCAN_SUGGEST=false
 image_hash=$(docker build ../src/consumer-apps/merge-csv-nodejs -t merge-csv-nodejs -q | sed -e 's/sha256\://g')
 echo $image_hash
 echo 'Generating Tar'
@@ -10,7 +11,8 @@ docker save $CUSTOMER_APP_NAME > $CUSTOMER_APP_NAME.tar
 echo 'Loading image into docker'
 docker load -i $CUSTOMER_APP_NAME.tar
 echo 'Tagging image with generated hash: '$image_hash
+echo docker tag 'sha256:'$image_hash $CUSTOMER_APP_NAME
 docker tag 'sha256:'$image_hash $CUSTOMER_APP_NAME
 echo 'Updating example-provider-routes.xml'
-sed -i 's/sha256-.*#8081/sha256-'${image_hash}'#8081/g' example-provider-routes.xml
+sed -i '' 's/sha256-.*#8081/sha256-'${image_hash}'#8081/g' example-provider-routes.xml
 echo 'Preparing application for UC - completed'
