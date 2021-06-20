a81b75129e61c28b811c21b9558f377e8d932fcb
Mayank Gautam <mayank@digitalgreen.org>
2021-06-21 04:08:37 +0530

    run.sh fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd fs-configs/usage-configs/usage-control-example/
+cd fs-configs/usage-control-example/
 docker-compose -f docker-compose-provider.yaml up -d
 docker-compose -f docker-compose-consumer.yaml up -d
