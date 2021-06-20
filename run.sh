b94d950bd6b0e11687be8c2f4510f076435bcf48
Mayank Gautam <mgautam099@gmail.com>
2021-06-21 04:24:59 +0530

    run and stop file changes

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 cd fs-configs/usage-control-example/
-docker-compose -f docker-compose-provider.yaml up -d
+docker-compose -f docker-compose-consumer.yaml down
+docker-compose -f docker-compose-provider.yaml down
 docker-compose -f docker-compose-consumer.yaml up -d
+docker-compose -f docker-compose-provider.yaml up -d
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd fs-configs/usage-control-example/
+docker-compose -f docker-compose-consumer.yaml down
+docker-compose -f docker-compose-provider.yaml down
