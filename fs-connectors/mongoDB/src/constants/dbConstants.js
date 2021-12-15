3920415d2252b0734d4166dc572d898d1a7d7415
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-15 15:17:12 +0530

    created constants and altered / route

---
+++ fs-connectors/mongoDB/src/constants/dbConstants.js
@@ -0,0 +1,5 @@
+export default db_constants = {
+  engines: {
+    mongodb: "mongoDB",
+  },
+};
--- fs-connectors/mongoDB/src/server.js
+++ fs-connectors/mongoDB/src/server.js
@@ -5,15 +5,25 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// middleware
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 // ejs
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    db_engines: db_constants.default.engines,
+    config: config,
+  });
 });
-
 
 // port config
 app.listen(port, () => {
