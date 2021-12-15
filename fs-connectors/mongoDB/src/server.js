53e3400ca3dc3c5e318591d118818b2f208f6972
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-15 15:05:37 +0530

    added view folder and created basic express app

--- fs-connectors/mongoDB/src/server.js
+++ fs-connectors/mongoDB/src/server.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const fs = require("fs-extra");
+const path = require("path");
 
 const app = express();
+const port = 3000;
+
+// ejs
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+// routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+// port config
+app.listen(port, () => {
+  console.log(`MongoDb connector app listening at http://localhost:${port}`);
+});
