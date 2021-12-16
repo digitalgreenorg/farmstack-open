da645fcb0c8b2b174a64caa12e01a37a4574653c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-16 11:58:54 +0530

    added and tested /testconnection

--- fs-connectors/mongoDB/src/server.js
+++ fs-connectors/mongoDB/src/server.js
@@ -5,6 +5,9 @@ const path = require("path");
 const db_constants = require("./constants/dbConstants");
 // import { config } from "./config";
 const config = require("./config");
+const Config = require("./config");
+
+var MongoClient = require("mongodb").MongoClient;
 
 const app = express();
 const port = 3000;
@@ -41,8 +44,66 @@ app.post("/configure", (req, res) => {
   };
   config.query.statement = req.body.query;
   fs.writeJSON("savedConfig.json", config);
-//   dbService = new DBService();
+  //   dbService = new DBService();
   res.redirect("/");
+});
+
+app.post("/test_configuration", async (req, res) => {
+  console.log(req.body);
+  const newConfig = {
+    db_engine: req.body.engine,
+    db: {
+      host: req.body.host,
+      port: req.body.port,
+      database: req.body.database,
+      user: req.body.user,
+      password: req.body.password,
+    },
+    // last_sync_time: config.last_sync_time,
+    query: {
+      statement: req.body.query,
+    },
+  };
+  let success;
+  try {
+    // let mongoUrlLocal = "mongodb://root:shani@localhost:27017";
+    let mongoUrl =
+      "mongodb://" +
+      `${newConfig.db.user}` +
+      ":" +
+      `${newConfig.db.password}` +
+      "@" +
+      `${newConfig.db.host}` +
+      ":" +
+      `${newConfig.db.port}`;
+    MongoClient.connect(
+      mongoUrl,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      (err, client) => {
+        if (err) {
+          console.log(err);
+          res.json({
+            success: false,
+            error: err,
+          });
+        } else {
+          console.log("connection successful!");
+          res.json({
+            success: true,
+          });
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.json({
+      success: false,
+      error: err,
+    });
+  }
 });
 
 // port config
