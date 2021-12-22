199eacfd2c509617b0969947fc7f1d1bafe26c55
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-12-22 16:06:45 +0530

    created provider mongodb routes xml file

--- fs-connectors/mysql/src/models/postgresql.model.ts
+++ fs-connectors/mysql/src/models/postgresql.model.ts
@@ -1,19 +1,19 @@
-import {Client, Pool} from "pg"
-import {DB} from "./baseDB.model"
+import { Client, Pool } from "pg";
+import { DB } from "./baseDB.model";
 
-export class PostGreSQL extends DB{
-    pool: Pool;
-    createPool(){
-        this.pool = new Pool(this.dbConfig);
-    }
-    
-    async fetch(sqlQuery: string){
-        const {rows} = await this.pool.query(sqlQuery);
-        return rows;
-    }
+export class PostGreSQL extends DB {
+  pool: Pool;
+  createPool() {
+    this.pool = new Pool(this.dbConfig);
+  }
 
-    async testConnection(): Promise<boolean>{
-        const result = this.fetch("SELECT NOW()");
-        return await result !== null
-    }
+  async fetch(sqlQuery: string) {
+    const { rows } = await this.pool.query(sqlQuery);
+    return rows;
+  }
+
+  async testConnection(): Promise<boolean> {
+    const result = this.fetch("SELECT NOW()");
+    return (await result) !== null;
+  }
 }
