29e988f219dd3afbc572562790ebbc073f92b979
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-01-05 15:17:42 +0530

    changed consumer merge csv to event driven consumer by add /trigger end pt

--- fs-connectors/consumer/merge-csv-nodejs/app.js
+++ fs-connectors/consumer/merge-csv-nodejs/app.js
@@ -1,43 +1,51 @@
 // Start REST server
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const axios = require("axios");
 
 const app = express();
-var data = []
+var data = [];
 // var data_count = 0;
 
 // just use JSON body data
-app.use(bodyParser.json({
-  inflate: true,
-  limit: '100mb'
-}));
+app.use(
+  bodyParser.json({
+    inflate: true,
+    limit: "100mb",
+  })
+);
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 // Start web page /
-app.get('/', function (req, res) {
-  if(data.length > 0)
-    res.render('home', {data:data})
-  else
-    res.send("Waiting for producer to send data.")
+app.get("/", function (req, res) {
+  if (data.length > 0) res.render("home", { data: data });
+  else res.send("Waiting for producer to send data.");
 });
 
 // Receive Message
-app.post('/post_data', function (req, res) {
-
+app.post("/post_data", function (req, res) {
   try {
     var req_body = req.body;
-    console.log('req_body ', JSON.stringify(req_body));
-    req_body.forEach(item => {
-      data.push(item)
+    console.log("req_body ", JSON.stringify(req_body));
+    req_body.forEach((item) => {
+      data.push(item);
     });
 
-    res.end('OK');
+    res.end("OK");
   } catch (e) {
     console.log(e);
   }
+});
+
+app.post("/trigger", async function (req, res) {
+  const query = req.body;
+  const result = await axios.post("http://consumer-core:9001/data/sync", {
+    query,
+  });
+  res.send("sent data");
 });
 
 var server = app.listen(8081, function () {
